c813bf9e32e7aadd473391eff37fbf35d0a01616
skaznik <skaznik@wszib.edu.pl>
2020-03-01 09:12:39 +0100

    typowanie

--- kompilacja.ts
+++ kompilacja.ts
@@ -1,1 +1,13 @@
-let x: string = 'Jakiś tekst'
+let x: string = 'Jakiś tekst';
+let y: number = 5;
+let z: boolean = true;
+
+//typowanie do dwóch
+
+let a: string | number = 5
+
+//typowanie tablicy
+let b: number[] = [1,2,3];
+
+//typowanie tablicy do dwóch rodzajów 9numer i string)
+let c: (number | string)[] = [1,2,'abc'];
